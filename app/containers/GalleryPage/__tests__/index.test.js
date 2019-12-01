78df773caedb514b78f0ff8bfa25e7c75e093603
Tobias Winkler <info@tobiaswinkler.berlin>
2019-12-01 20:38:14 +0100

    fixed snapshot for connected component
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- app/containers/GalleryPage/__tests__/index.test.js
+++ app/containers/GalleryPage/__tests__/index.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import history from 'utils/history';
 import { render } from 'react-testing-library';
 
 import GalleryPage from '../index';
+import configureStore from '../../../configureStore';
+
+const initialState = {};
+const store = configureStore(initialState, history);
 
 describe('<GalleryPage />', () => {
   it('should render and match the snapshot', () => {
     const {
       container: { firstChild },
-    } = render(<GalleryPage />);
+    } = render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <GalleryPage />
+        </ConnectedRouter>
+      </Provider>,
+    );
     expect(firstChild).toMatchSnapshot();
   });
 });
